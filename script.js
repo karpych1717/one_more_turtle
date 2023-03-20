0f8a0e8d66f7cfb3600b52dd1a2aca8fd8d53f26
karpych1717 <admkarpych1717@gmail.com>
2023-03-20 12:59:29 +0200

    codestyle fix2

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-for(let i = 0; i < 360; i++) {
+for (let i = 0; i < 360; i++) {
   forward(1)
   right(1)
 }
 
-for(let i = 0; i < 360; i++) {
+for (let i = 0; i < 360; i++) {
   forward(1)
   left(1)
 }
 
 left()
 
-for(let i = 0; i < 360; i++) {
+for (let i = 0; i < 360; i++) {
   forward(1)
   left(1)
 }
 
-for(let i = 0; i < 360; i++) {
+for (let i = 0; i < 360; i++) {
   forward(1)
   right(1)
 }
